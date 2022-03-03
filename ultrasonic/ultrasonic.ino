b0433d0f852300d42fefaa0dca07592381e5ddf6
aidan <aidanhoward757@gmail.com>
2022-03-03 09:09:37 -0800

    updated info and source

---
+++ ultrasonic/ultrasonic.ino
@@ -0,0 +1,12 @@
+// https://create.arduino.cc/projecthub/knackminds/how-to-measure-distance-using-ultrasonic-sensor-hc-sr04-a-b9f7f8
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
